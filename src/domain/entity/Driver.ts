eb6f85a4a3b897fb23a3c5b24dffeae98890af69
Jonathan Urrego <jadougdev@gmail.com>
2025-02-09 14:21:20 -0500

    add builder pattern and valueObjects/phone

--- src/domain/entity/Driver.ts
+++ src/domain/entity/Driver.ts
@@ -1,9 +1,9 @@
 import { User } from "./User";
+import { UserRole } from "../enum/UserRole";
+import { Phone } from "../valueObject/Phone";
 
 export class Driver extends User {
-    
-    constructor(id: string, identification: number, name: string, phone:string){
-        super(id, identification, name, phone);
+    constructor(id: string, identification: number, name: string, phone: Phone){
+        super(id, identification, name, UserRole.DRIVER, phone);
     }
-
 }
--- src/domain/entity/User.ts
+++ src/domain/entity/User.ts
@@ -1,13 +1,17 @@
+import { UserRole } from "../enum/UserRole";
+import { Phone } from "../valueObject/Phone";
+
 export class User {
     id: string;
     identification: number;
     name: string;
-    phone?: string;
+    role: UserRole;
+    phone?: Phone;
   
-    constructor(id: string, identification: number, name: string, phone?: string) {
-      this.id = id;
+    constructor(id: string, identification: number, name: string, role: UserRole, phone?: Phone) {
       this.identification = identification;
       this.name = name;
+      this.role = role;
       this.phone = phone;
     }
 
@@ -19,8 +23,16 @@ export class User {
         return this.identification;
     }
 
+    public getRole(): UserRole {
+        return this.role;
+    }
+
+    public getPhone(): string | undefined {
+        return this.phone?.getValue();
+    }
 
     public getName(): string {
         return this.name;
     }
 }
+
